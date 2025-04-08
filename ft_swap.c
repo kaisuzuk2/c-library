595b14edcd428b8561e81bab7a750cffdf1dc150
kaisuzuk <szkakg@gmail.com>
2025-04-08 10:39:06 +0900

    add ft_swap

---
+++ ft_swap.c
@@ -0,0 +1,21 @@
+#include "ft.h"
+#include <stdio.h>
+
+void ft_swap(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(void)
+{
+    int a, b;
+    a = 5;
+    b = 3;
+
+    ft_swap(&a, &b);
+    printf("%d %d", a, b);
+}
