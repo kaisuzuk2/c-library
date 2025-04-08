1fc6f8e1adab69adac9ee8fb65f35a7621b509bb
kaisuzuk <szkakg@gmail.com>
2025-04-08 09:34:55 +0900

    add ft_putnbr.c

---
+++ ft_putnbr.c
@@ -0,0 +1,18 @@
+#include "ft.h"
+
+void ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
+}
